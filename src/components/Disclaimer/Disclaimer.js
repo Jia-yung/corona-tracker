f107c4c5865f9c54b846aa6bea736453ed45a09c
Yap Jia Yung <yapjiayung@gmail.com>
2020-04-07 11:36:31 +0800

    added meta image for whatsapp size, changed disclaimer, update css

--- src/components/Disclaimer/Disclaimer.js
+++ src/components/Disclaimer/Disclaimer.js
@@ -5,7 +5,17 @@ const disclaimer = () => (
     <div className="disclaimerContainer">
         <h4>About the data</h4>
         <h6>Rapid Changes</h6>
-        <p>The data displayed on this site may not always be up to date. Relevant information about reported cases can be found on <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports" target="_blank" rel="noopener noreferrer">World Health Organization (WHO) official site.</a></p>
+        <p>
+            The data displayed on this site may not always be up to date. Relevant information about reported cases can be found on   
+            <a className="links" href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports" target="_blank" rel="noopener noreferrer">
+                World Health Organization (WHO)  
+            </a>
+            and
+            <a className="links" href="https://coronavirus.jhu.edu/" target="_blank" rel="noopener noreferrer" >
+                John Hopkins University (JHU)
+            </a>
+            official site.
+        </p>
     </div>
 )
 
