704731e6901ab1a922e409051e6e728d7f44be45
Yap Jia Yung <yapjiayung@gmail.com>
2020-04-19 15:20:16 +0800

    removed show refresh button

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -9,7 +9,6 @@ class DataTable extends Component {
                     data-url= 'https://corona.lmao.ninja/v2/countries?sort=cases'                
                     data-toggle="table"
                     data-sticky-header = "true"
-                    data-show-refresh="true"
                     data-height = "500"
                     data-classes="table table-bordered table-dark table-striped"
                     data-search="true" >
