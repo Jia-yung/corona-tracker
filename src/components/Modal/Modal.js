4e11e1b01ab4eea346dab8b88e4101a8d731fb9d
Yap Jia Yung <yapjiayung@gmail.com>
2020-04-25 20:30:57 +0800

    Update Modal.js
    -Added percentage figure

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,6 +4,7 @@ import './Modal.css'
 
  class modal extends Component {
     render () {
+        let incrementPercentage = (this.props.today/this.props.figure) * 100
         return (
             <div className="modalContainer">
                 <div style={{display: this.props.showSpinner? 'none' : 'block'}}>
@@ -11,13 +12,23 @@ import './Modal.css'
                         <h4 className="arrow">↑</h4>
                         <h4 className="null" style={{display: this.props.today? 'none' : 'block'}}>--</h4>
                         <CountUp 
-                            style={{display: this.props.showSpinner? 'none' : 'block'}} 
+                            style={{display: this.props.today? 'block' : 'none'}} 
                             className={["figureIncrement", this.props.status].join(' ')} 
                             duration={2.5} 
                             separator="," 
                             start={0} 
                             end={this.props.today} 
                             delay={0} />
+                        <CountUp 
+                            style={{display: this.props.today? 'block' : 'none'}} 
+                            className= "figureIncrement percent"
+                            duration={2.5} 
+                            decimals={2}
+                            start={0} 
+                            end={incrementPercentage}
+                            prefix="±"
+                            suffix="%"
+                            delay={0} />            
                     </div>
                     <CountUp 
                         className={["figure", this.props.status].join(' ')} 
