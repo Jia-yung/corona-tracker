c78eeb7fddc117c95a1ed2b4231599e8f10785f5
Yap Jia Yung <yapjiayung@gmail.com>
2020-04-27 22:04:11 +0800

    Update list item.js

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -7,24 +7,30 @@ class ListItem extends Component {
         let category = "infected"
 
         if (this.props.sortBy === "country" || this.props.sortBy === "cases") {
-            category = "infected"
+            category = "Infected"
             figure = this.props.cases
         } else if (this.props.sortBy === "deaths") {
-            category = "deaths"
+            category = "Death"
             figure = this.props.deaths
         } else if (this.props.sortBy === "recovered") {
-            category = "recovered"
+            category = "Recovered"
             figure = this.props.recovered
+        } else if (this.props.sortBy === "recoveryRate") {
+            category = "Recovery rate"
+            figure = (this.props.recoveryRate/100).toString() + "%"
+        } else if (this.props.sortBy === "deathRate") {
+            category = "Death rate"
+            figure = (this.props.deathRate/100).toString() + "%"
         }
         return (
             <li onClick={this.props.clicked}>
                 <h6>
-                    <img className="itemFlag" src={this.props.flag} alt="flag" align="middle" />
+                    <img className="itemFlag" src={this.props.flag} alt="" align="middle" />
                     {this.props.country}
                 </h6>
                 <div className="listContent">
+                    <h6>{category + ":"}</h6>
                     <h6 className={category} >{figure.toLocaleString()}</h6>
-                    <h6>{category}</h6>
                 </div>
             </li>
         )
